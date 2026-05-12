07577e6640163e059bc5f17c582a1291c086e82f
Benny <asshimbenny01@gmail.com>
2026-05-12 12:43:49 +0300

    first commit

--- react-based-project/src/pages/AdminPortal.jsx
+++ react-based-project/src/pages/AdminPortal.jsx
@@ -5,3 +5,19 @@ import Shop from "./pages/Shop";
 import AdminPortal from "./pages/AdminPortal";
 import { CoffeeProvider } from "./context/CoffeeContext";
 import "./App.css";
+
+export default function App() {
+  const [activePage, setActivePage] = useState("home");
+
+  const renderPage = () => {
+    switch (activePage) {
+      case "home":
+        return <Home onShopClick={() => setActivePage("shop")} />;
+      case "shop":
+        return <Shop />;
+      case "admin":
+        return <AdminPortal />;
+      default:
+        return <Home />;
+    }
+  };
